8181a0f22c9eca8f67727b3ad73587f9f563a7ab
utkarsh06 <utkarsh.gaur06@gmail.com>
2021-01-14 15:42:17 -0500

    Added Navigation Bar

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
 
 import './App.css';
 import React from 'react';
+import {BrowserRouter as Router, Route, Link} from 'react-router-dom';
+import Container from 'react-bootstrap/Container';
+import Navbar from 'react-bootstrap/Navbar';
+import { NavbarBrand } from 'react-bootstrap';
+import Nav from 'react-bootstrap/Nav'
 
 class App extends React.Component {
   constructor(props) {
@@ -29,7 +34,22 @@ class App extends React.Component {
   
   render() {
     return (
-      <div>Hello</div>
+      <Router>
+        <Container className = "p-0" fluid = {true}>
+          <navbar className = "border-bottom">
+            <Navbar.Brand>Utkarsh Gaur </Navbar.Brand>
+            <Navbar.Toggle aria-controls = "navbar-toggle"></Navbar.Toggle>
+            <Navbar.Collapse aria-controls = "navbar-toggle">
+              <Nav>
+                <Link className = "nav-link" to = "">Home</Link>
+                <Link className = "nav-link" to = "/about">About</Link>
+                <Link className = "nav-link" to = "/contact">Contact</Link>
+              </Nav>
+            </Navbar.Collapse>
+          </navbar>
+        </Container>
+      </Router>
+
     );
   }
 }
